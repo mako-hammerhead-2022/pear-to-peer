7097f52203af70f9a008eefb00869729f3e4ac4e
Mat Ruane <8108538+matruane@users.noreply.github.com>
2022-07-13 19:07:14 +1200

    Update with Rohan's eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,62 @@
+// module.exports = {
+//   extends: ['eda/react'],
+//   parser: '@typescript-eslint/parser',
+//   ignorePatterns: ['**/node_modules/**', '**/dist/**'],
+//   settings: {
+//     'import/resolver': {
+//       alias: {
+//         map: [
+//           ['@', './client'],
+//           ['~', '.'],
+//         ],
+//         extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+//       },
+//     },
+//   },
+// }
 module.exports = {
-  extends: ['eda/react'],
   parser: '@typescript-eslint/parser',
-  ignorePatterns: ['**/node_modules/**', '**/dist/**'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   settings: {
+    react: { version: 'detect' },
     'import/resolver': {
+      node: {
+        extensions: ['.js'],
+      },
       alias: {
         map: [
           ['@', './client'],
-          ['~', '.'],
+          ['@test', './test'],
         ],
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
       },
+      extensions: ['.js', '.jsx', '.css'],
     },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+    'vitest-globals/env': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:vitest-globals/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  plugins: ['simple-import-sort', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 }
