cdff83664a0dd8d736dbf72e24773bed2abe2ecf
Mat Ruane <8108538+matruane@users.noreply.github.com>
2022-07-15 10:19:52 +1200

    not-working validation stuff

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -8,7 +8,10 @@ import {
   FormLabel,
   Heading,
   Input,
+  NumberInput,
+  NumberInputField,
 } from '@chakra-ui/react'
+import * as Yup from 'yup'
 import { postNewUser } from '../slices/usersSlice'
 
 export default function Register() {
@@ -38,6 +41,21 @@ export default function Register() {
     navigate('/')
   }
 
+  const RegisterSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(32, 'Too Long!')
+      .required('Required'),
+    username: Yup.string()
+      .min(2, 'Too Short!')
+      .max(16, 'Too Long!')
+      .required('Required'),
+    postcode: Yup.number()
+      .min(110, 'Too low for a postcode!')
+      .max(9999, 'Too high for a postcode!')
+      .required('Required'), //0110 - 9999
+  })
+
   //TODO: handle loading state
 
   return (
@@ -46,6 +64,7 @@ export default function Register() {
       {form.auth0Id != '' ? (
         <Formik
           initialValues={{ ...form, name: '', username: '', postcode: '' }}
+          validationSchema={RegisterSchema}
           onSubmit={(values) => handleSubmit(values)}
         >
           {(props) => (
@@ -79,7 +98,7 @@ export default function Register() {
               </Field>
               <Field name='name'>
                 {({ field, form }) => (
-                  <FormControl>
+                  <FormControl isRequired>
                     <FormLabel htmlFor='name'>Your name:</FormLabel>
                     <Input {...field} id='name'></Input>
                   </FormControl>
@@ -87,7 +106,7 @@ export default function Register() {
               </Field>
               <Field name='username'>
                 {({ field, form }) => (
-                  <FormControl>
+                  <FormControl isRequired>
                     <FormLabel htmlFor='username'>Display name:</FormLabel>
                     <Input {...field} id='username'></Input>
                   </FormControl>
@@ -95,11 +114,20 @@ export default function Register() {
               </Field>
               {/* TODO: validate postcode, only allow numbers */}
               <Field name='postcode'>
-                {({ field, form }) => (
-                  <FormControl>
-                    <FormLabel htmlFor='postcode'>Postal Code:</FormLabel>
-                    <Input {...field} id='postcode'></Input>
-                  </FormControl>
+                {props.errors.postcode && props.touched.postcode ? (
+                  <div>{props.errors.postcode}</div>
+                ) : (
+                  ({ field, form }) => (
+                    <FormControl isRequired>
+                      <FormLabel htmlFor='postcode'>Postal Code:</FormLabel>
+                      <NumberInput id='postcode' max={9999} min={100}>
+                        <NumberInputField
+                          {...field}
+                          id='postcode'
+                        ></NumberInputField>
+                      </NumberInput>
+                    </FormControl>
+                  )
                 )}
               </Field>
               <Button mt={4} colorScheme='teal' type='submit'>
