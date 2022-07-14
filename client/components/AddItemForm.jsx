2f5fd998167250a0cdc7d8a40105d4fc4284b83a
Jojo Mepham <103151844+Jojo-Mepham@users.noreply.github.com>
2022-07-15 11:10:27 +1200

    Get addItem data from form to handleSubmit

--- client/components/AddItemForm.jsx
+++ client/components/AddItemForm.jsx
@@ -1,106 +1,146 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { addItem } from '../apiClient/items'
-import { Formik, Form } from 'formik'
+import { Formik, Form, Field } from 'formik'
+import {
+  FormLabel,
+  Input,
+  Button,
+  Container,
+  Select,
+  Textarea,
+  FormControl,
+} from '@chakra-ui/react'
 
 export function AddItemForm() {
   const item = useSelector((state) => state.itemData)
-  const [itemForm, setItemForm] = useState([])
+  //const [itemForm, setItemForm] = useState([])
 
-  useEffect(
-    (items) => {
-      setItemForm(items)
-    },
-    [item]
-  )
+  // useEffect(
+  //   (items) => {
+  //     setItemForm(items)
+  //   },
+  //   [item]
+  // )
 
-  async function handleClick() {
-    const itemToAdd = {
-      itemName: item.itemName,
-      allergens: item.allergens,
-      descripton: item.description,
-      expiry: item.expiry,
-      availability: item.availability,
-    }
-    await addNewItem(itemForm)
+  async function handleSubmit(formData) {
+    console.log('submitting', formData)
+    // const itemToAdd = {
+    //   itemName: item.itemName,
+    //   allergens: item.allergens,
+    //   descripton: item.description,
+    //   expiry: item.expiry,
+    //   availability: item.availability,
+    // }
+    // await addNewItem(itemForm)
   }
 
   return (
     <Formik
-      initialValues={{}}
+      initialValues={{
+        itemName: '',
+        allergens: '',
+        description: '',
+        image: '',
+        availability: 'yes',
+      }}
       onSubmit={(values) => {
-        onSubmit(values)
-        handleSubmit()
+        handleSubmit(values)
       }}
     >
-      {(props) => (
-        <Form>
-          <FormLabel htmlFor='itemName'>Item Name:</FormLabel>
-          <Input
-            type='text'
-            id='itemName'
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-            required
-          />
-          <FormLabel htmlFor='allergens'>Allergens:</FormLabel>
-          <Input
-            type='text'
-            id='allergens'
-            value={allergens}
-            onChange={(e) => setAllergens(e.target.value)}
-            required
-          />
-          <FormLabel htmlFor='description'>Description of food item:</FormLabel>
-          <Textarea
-            name='description'
-            id='description'
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            required
-          ></Textarea>
-          <FormLabel htmlFor='image'>Upload image:</FormLabel>
-          <Input
-            type='file'
-            name='image'
-            id='image'
-            accept='image/*'
-            multiple
-            // onChange={handleFileChange}
-          />
-          <FormLabel htmlFor='availability'>Is this item available?</FormLabel>
-          <Select
-            name='availability'
-            id='availability'
-            value={availability}
-            onChange={(e) => setAvailability(e.target.value)}
-            required
-          >
-            <option value='yes'>Yes</option>
-            <option value='no'>No</option>
-          </Select>
+      {(props) => {
+        const { itemName, allergens, description, image, availability } =
+          props.values
+        return (
+          <Form>
+            <Field name='itemName'>
+              {({ field }) => (
+                <FormControl>
+                  <FormLabel htmlFor='itemName'>Item Name:</FormLabel>
+                  <Input {...field} type='text' id='itemName' required />
+                </FormControl>
+              )}
+            </Field>
+            <Field name='allergens'>
+              {({ field }) => (
+                <FormControl>
+                  <FormLabel htmlFor='allergens'>Allergens:</FormLabel>
+                  <Input {...field} type='text' id='allergens' required />
+                </FormControl>
+              )}
+            </Field>
+            <Field name='description'>
+              {({ field }) => (
+                <FormControl>
+                  <FormLabel htmlFor='description'>
+                    Description of food item:
+                  </FormLabel>
+                  <Textarea {...field} type='text' id='description' required />
+                </FormControl>
+              )}
+            </Field>
+            <Field name='image'>
+              {({ field }) => (
+                <FormControl>
+                  <FormLabel htmlFor='image'>Upload image:</FormLabel>
+                  <Input
+                    {...field}
+                    type='file'
+                    name='image'
+                    id='image'
+                    accept='image/*'
+                    multiple
+                    // onChange={handleFileChange}
+                  />
+                </FormControl>
+              )}
+            </Field>
 
-          <Container centerContent>
-            <div>
-              <Button
-                type='submit'
-                isLoading={props.isSubmitting}
-                colorScheme='teal'
-                m={2}
-                onClick={props.handleClick}
-              >
-                Add Item
-              </Button>
-            </div>
-          </Container>
-        </Form>
-      )}
+            <FormLabel htmlFor='availability'>
+              Is this item available?
+            </FormLabel>
+            <Field name='itemName'>
+              {({ field }) => (
+                <FormControl>
+                  <Select
+                    name='availability'
+                    id='availability'
+                    defaultValue={availability}
+                    required
+                  >
+                    <option {...field} value='yes'>
+                      Yes
+                    </option>
+                    <option {...field} value='no'>
+                      No
+                    </option>
+                  </Select>
+                </FormControl>
+              )}
+            </Field>
+
+            <Container centerContent>
+              <div>
+                <Button
+                  type='submit'
+                  isLoading={props.isSubmitting}
+                  colorScheme='teal'
+                  m={2}
+                  onClick={props.handleSubmit}
+                >
+                  Add Item
+                </Button>
+              </div>
+            </Container>
+          </Form>
+        )
+      }}
     </Formik>
   )
 }
 
-addItem.defaultProps = {
-  onSubmit: () => {},
-}
+// addItem.defaultProps = {
+//   onSubmit: () => {},
+// }
 
 export default addItem
--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AddItemForm } from '@/Components/AddItemForm'
+import { AddItemForm } from '../components/AddItemForm'
 
 export default function Profile() {
   return (
