0194f0ec6ef75bdc80245241bf6c01211b9e0869
luis-leh <103152911+luis-leh@users.noreply.github.com>
2022-07-15 11:05:06 +1200

    get items into redux state

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+
+import { fetchAllItems } from '../slices/itemSlice'
 
 export default function Home() {
-  return <div>Home</div>
+  const items = useSelector((state) => console.log(state))
+  const dispatch = useDispatch()
+  console.log('items', items)
+
+  useEffect(() => {
+    dispatch(fetchAllItems())
+  }, [])
+  // return <Grid templateColumns='repeat(4, 1fr)'></Grid>
+  return (
+    <div>
+      <h1>Here are the Items</h1>
+      <div></div>
+    </div>
+  )
 }
--- client/slices/itemSlice.js
+++ client/slices/itemSlice.js
@@ -1,7 +1,13 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { addItem } from '@/apiClient/items'
+import { getAllItems, addItem } from '@/apiClient/items'
 
-const initialState = {}
+const initialState = { items: [] }
+
+export const fetchAllItems = createAsyncThunk('items/fetchAll', async () => {
+  const response = await getAllItems()
+  console.log('response is', response)
+  return response
+})
 
 export const postNewItem = createAsyncThunk('items/postNew', async (item) => {
   const response = await addItem(item)
@@ -13,12 +19,19 @@ export const itemSlice = createSlice({
   name: 'itemData',
   initialState,
   reducers: {
+    // setAllItems: (state, {payload}) => {
+    //   return {...state, }
+    // },
     setNewItem: (state, { payload }) => {
       return { ...state, ...payload.userToSave }
     },
   },
 
   extraReducers: {
+    [fetchAllItems.fulfilled]: (state, { payload }) => {
+      console.log('payload', payload)
+      return { ...state, items: payload }
+    },
     [postNewItem.fulfilled]: (state, { payload }) => {
       console.log('postItemPayload', payload)
       return { ...state, items: payload }
