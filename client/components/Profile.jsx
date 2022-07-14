a4d909748fb11474068440a65ed9dd6e141240e2
charlotte-greatbatch <103108567+charlotte-greatbatch@users.noreply.github.com>
2022-07-14 22:24:09 +1200

    addItemForm

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { AddItemForm } from '@/Components/AddItemForm'
 
 export default function Profile() {
   return (
     <>
+      <AddItemForm />
       <h1>Hey this is a profile page</h1>
     </>
   )
