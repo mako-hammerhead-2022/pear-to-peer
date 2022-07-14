d7c043ed712cc11be49a9103e01c8f96fd8efbca
charlotte-greatbatch <charlottegreatbatch01@gmail.com>
2022-07-14 15:57:08 +1200

    starting item slice

---
+++ client/slices/itemSlice.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { getAllItems, addItem } from '@/apiClient/users'
+
+const initialState = { items: [], loading: false, error: null }
+
+export const postNewItem = createAsyncThunk('items/postNew', async (item) => {
+  const response = await addItem(item)
+  console.log('addItem response', response)
+  return response
+})
+
+export const itemSlice = createSlice({
+  name: 'item',
+  initialState,
+  reducers: {
+    setNewItem: (state, action) => {
+      state.items = action.payload
+    },
+  },
+
+  extraReducers: {
+    [postNewItem.fulfilled]: (state, { payload }) => {
+      console.log('postItemPayload', payload)
+      return { ...state, items: payload }
+    },
+  },
+})
+export const { setNewItem } = itemsSlice.actions
+
+export default itemsSlice.reducer
