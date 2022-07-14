4c739c7cf945c71a57377070a1d8d3cc5c35f216
charlotte-greatbatch <103108567+charlotte-greatbatch@users.noreply.github.com>
2022-07-15 11:36:45 +1200

    get items rendering on home component

--- client/components/FoodItemTile.jsx
+++ client/components/FoodItemTile.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Box, Heading, Text, Image } from '@chakra-ui/react'
 
-export default function FoodItemTile() {
+export default function FoodItemTile(props) {
+  console.log(props, 'props')
+  const { imageUrl, itemName, allergens, description, expiry, availability } =
+    props.data
+  console.log(imageUrl)
   return (
     <>
-      <Grid templateColumns='repeat(4, 1fr)'></Grid>
+      <Box>
+        <Image src={imageUrl} />
+        <Heading>{itemName}</Heading>
+        <Text>Allergens: {JSON.parse(allergens).join(', ')}</Text>
+        <Text>Description: {description}</Text>
+        <Text>{expiry}</Text>
+        {/* <Text>{availability}</Text> */}
+      </Box>
     </>
   )
 }
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,19 +4,31 @@ import { useSelector, useDispatch } from 'react-redux'
 
 import { fetchAllItems } from '../slices/itemSlice'
 
+import { Container, Grid, GridItem, Heading } from '@chakra-ui/react'
+import FoodItemTile from './FoodItemTile'
+
 export default function Home() {
-  const items = useSelector((state) => console.log(state))
+  const items = useSelector((state) => state.itemData.items)
+
   const dispatch = useDispatch()
   console.log('items', items)
 
   useEffect(() => {
     dispatch(fetchAllItems())
   }, [])
-  // return <Grid templateColumns='repeat(4, 1fr)'></Grid>
+  // return
   return (
-    <div>
-      <h1>Here are the Items</h1>
-      <div></div>
-    </div>
+    <Container>
+      <Heading>ITEMS ARE HERE</Heading>
+      <Grid templateColumns='repeat(4, 1fr)'>
+        {items.map((item) => {
+          return (
+            <GridItem gap='50px'>
+              <FoodItemTile key={item.id} data={item} />
+            </GridItem>
+          )
+        })}
+      </Grid>
+    </Container>
   )
 }
