63e94cda20b38ce303037c43c939a38e9c6756b4
charlotte-greatbatch <103108567+charlotte-greatbatch@users.noreply.github.com>
2022-07-19 07:26:39 +0800

    getting ready for pull

--- client/apiClient/__tests__/users.test.js
+++ client/apiClient/__tests__/users.test.js
@@ -1,11 +1,11 @@
 import nock from 'nock'
 
-import { addUser, getUserByAuth0Id } from '../users'
+import { addUser, getUserByAuth0Token } from '../users'
 
 // ADD A USER
 
 describe('POST /api/users', () => {
-  // expect.assertions(5)
+  expect.assertions(3)
   test('inserts a new user', async () => {
     const newUser = {
       auth0Id: 'auth0|4',
@@ -20,21 +20,21 @@ describe('POST /api/users', () => {
       .reply(201, newUser)
 
     const userRes = await addUser(newUser)
-    console.log('THIS IS USER RES: ', userRes)
     expect(userRes.content).toBe(newUser.content)
-
+    expect(userRes.auth0Id).toContain('auth0|4')
+    expect(userRes.name).toContain('Perry Platipus')
     scope.done()
   })
 })
 
 // GET USER BY AUTH ID
 
-describe("GET user by the user's auth Id", () => {
+describe("GET user by the user's auth token", () => {
   expect.assertions(4)
-  test.skip("gets a user by the user's auth Id", async () => {
+  test("gets a user by the user's auth token", async () => {
     const testUser = {
       id: 13,
-      auth0Id: 'auth0|4',
+      auth0Id: 'auth0|4token',
       name: 'Perry Platipus',
       username: 'PTP123',
       email: 'ptp123@doofenshmirtz.com',
@@ -47,7 +47,7 @@ describe("GET user by the user's auth Id", () => {
       .get(`/api/users/${authId}`)
       .reply(200, testUser)
 
-    const userRes = await getUserByAuth0Id(authId)
+    const userRes = await getUserByAuth0Token(authId)
     expect(userRes.id).toBe(13)
     expect(userRes.name).toContain('Perry Platipus')
     expect(userRes.username).toContain('PTP123')
